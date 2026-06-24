4cb1bc0883ac06347d89eb5ff8c1d04d8cf3c6a8
Lithasa Jayamaha <lithasa0417@gmail.com>
2026-06-24 12:53:34 +0530

    Prepare backend for Render hosting

--- backend/WasteGlass.Api/Program.cs
+++ backend/WasteGlass.Api/Program.cs
@@ -4,6 +4,9 @@ using WasteGlass.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5057";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 builder.Services.AddControllers();
 builder.Services.AddScoped<IRouteService, RouteService>();
 
--- backend/WasteGlass.Api/obj/Debug/net8.0/WasteGlass.Api.AssemblyInfo.cs
+++ backend/WasteGlass.Api/obj/Debug/net8.0/WasteGlass.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WasteGlass.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67dfedb974d1a5110924d632b8b501c523f9d94e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0614cb04f5c4d0072f3ca0c348ccf51bb939392d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WasteGlass.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WasteGlass.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
